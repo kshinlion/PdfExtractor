6c3cee70e90f25e4564c071af7abe0934634c1d5
K.S. Lion <135800895+kshinlion@users.noreply.github.com>
2025-10-20 21:13:04 +0700

    feat: Extract text
    Extract text from pdf and write to output.txt

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using UglyToad.PdfPig;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    string inputPath = "C:/General Files/Projects/Travel Expenses/08. Invoice Aug/Mr Changman Jo #3310 31 Aug - 30 Sep 2025.pdf";
+    string outputPath = "output.txt";
+
+
+    string extractedText = "";
+
+    using (var document = PdfDocument.Open(inputPath))
+    {
+      foreach (var page in document.GetPages())
+      {
+        extractedText += page.Text + Environment.NewLine;
+      }
+    }
+
+    File.WriteAllText(outputPath, extractedText);
+    Console.WriteLine($"Text extracted to: {outputPath}");
+  }
+}
--- obj/Debug/net9.0/PdfExtractor.AssemblyInfo.cs
+++ obj/Debug/net9.0/PdfExtractor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PdfExtractor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64b0d3454a965718abc1d3639dd83df16f437ffb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PdfExtractor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PdfExtractor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
